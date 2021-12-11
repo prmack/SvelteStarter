b47a537a5b40bd0a8b53dbacf4e293cd9664b3b3
Paul Mackenzie <paul@whatspauldoing.com>
2021-12-11 13:37:12 +0000

    Updating packages, adding lang attribute to components

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,15 @@
-<script>
-	import { Router } from "svelte-router-spa";
-	import { routes } from "./routes";
+<script lang="ts">
+  import { Router } from "svelte-router-spa";
+  import { routes } from "./routes";
 </script>
 
-<style global>
-	@import "src/styles/globalStyles.scss";
-	@import "bulma/sass/utilities/_all.sass";
-	@import "bulma/sass/grid/columns.sass";
-	@import "bulma/sass/elements/container.sass";
+<style global lang="scss">
+  @import "src/styles/globalStyles.scss";
+  @import "bulma/sass/utilities/_all.sass";
+  @import "bulma/sass/grid/columns.sass";
+  @import "bulma/sass/elements/container.sass";
 </style>
 
 <div class="container">
-	<Router {routes} />
+  <Router {routes} />
 </div>
